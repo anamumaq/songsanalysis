4d295b57e4911452aca1dc473eef431cf504400a
anamumaq <ana.munozmaquera@gmail.com>
2021-05-18 01:10:16 -0500

    Resultados analisis de sentimiento Breakout album

--- script.R
+++ script.R
@@ -2,8 +2,6 @@ library(genius)
 library(tidyverse)
 library(tidytext) #for text tidying
 
-MC_Breakout_tracks = genius_tracklist("Miley Cyrus", "Hannah Montana 2")
-MC_Breakout_lyrics = genius_album(artist = "Miley Cyrus")#, album = "Breakout"
 # albunes
 # 9. Plastic hearts 2020
 # 8. She is coming 2019 
@@ -16,58 +14,134 @@ MC_Breakout_lyrics = genius_album(artist = "Miley Cyrus")#, album = "Breakout"
 # 2. Breakout 2008
 # 1. Hannah Montana 2: Meet Miley Cyrus 2007
 
-a1 = genius_tracklist(artist = "Miley Cyrus", album = "Meet Miley Cyrus")%>%
-  mutate(evento = "End of Hannah Montana", year = 2007)
-a2 = genius_tracklist(artist = "Miley Cyrus", album = "Breakout")%>%
-  mutate(evento = "Beak out con Nick Jonas", year = 2008)
-a3 = genius_tracklist(artist = "Miley Cyrus", album = "The time of our lives")%>%
-  mutate(evento = "worse celebrity influence", year = 2009)
-a4 = genius_tracklist(artist = "Miley Cyrus", album = "Can't be tamed")%>%
-  mutate(evento = "First independed album", year = 2010)
-a5 = genius_tracklist(artist = "Miley Cyrus", album = "Bangerz")%>%
-  mutate(evento = "VMA performance twerk", year = 2013)
-a6 = genius_tracklist(artist = "Miley Cyrus", album = "Miley Cyrus and her dead Petz")%>%
-  mutate(evento = "VMA performance transparent outfit", year = 2015)
-a7 = genius_tracklist(artist = "Miley Cyrus", album = "Younger now")%>%
-  mutate(evento = "Change style", year = 2017)
-a8 = genius_tracklist(artist = "Miley Cyrus", album = "She is coming")%>%
-  mutate(evento = "Divorce", year = 2019)
-a9 = genius_tracklist(artist = "Miley Cyrus", album = "Plastic hearts")%>%
-  mutate(evento = "Divorce", year = 2020)
 
-Album =  rbind(a1, a2, a3, a4, a5, a6, a7, a8, a9)
-
-t1 = genius_album(artist = "Miley Cyrus", album = "Meet Miley Cyrus")%>%
-  mutate(album = "Taylor Swift")
+# 1. Hannah Montana 2: Meet Miley Cyrus 2007
 
 
-# canciones sin letras :()
-head(filter(miley_1, is.na(lyric)))
+t1 = genius_album(artist = "Miley Cyrus", album = "Meet Miley Cyrus")
 
-## para las letras que no encuentra directamente hay que obtener el url y buscarlo por ahi
-flyonthewall = MC_Breakout_tracks %>% 
-  filter(track_title == "Fly on the Wall")
+#unique(t1$track_title)
 
-MC_flyonthewall = genius_url(flyonthewall$track_url, info = "all") #Simple regresa la letra de la URL
+pend_t1 = filter(t1, is.na(line)) # obtengo las canciones que no tienen letras
 
-  # MC_flyonthewall = genius_lyrics(artist = "Miley Cyrus", song = "Fly on the Wall") #resultado NA
+pend_song =  genius_tracklist(artist = "Miley Cyrus", album = "Meet Miley Cyrus")%>% 
+  filter(track_title %in% pend_t1$track_title) # obtengo url de las canciones sin letras
 
-MC_1 = MC_Breakout_lyrics%>%
+# obtengo las letras de las canciones que faltaban uno a uno
+a1_1 = genius_url(pend_song$track_url[1], info = "all")
+a1_2 = genius_url(pend_song$track_url[2], info = "all")
+a1_3 = genius_url(pend_song$track_url[3], info = "all")
+a1_4 = genius_url(pend_song$track_url[4], info = "all")
+a1_5 = genius_url(pend_song$track_url[5], info = "all")
+a1_6 = genius_url(pend_song$track_url[6], info = "all")
+a1_7 = genius_url(pend_song$track_url[7], info = "all")
+
+# uno los data sets en un solo llamado como el albun
+HM = bind_rows(t1[c("track_title", "lyric")], 
+          a1_1[c("track_title", "lyric")], a1_2[c("track_title", "lyric")],
+          a1_3[c("track_title", "lyric")], a1_4[c("track_title", "lyric")],
+          a1_5[c("track_title", "lyric")], a1_6[c("track_title", "lyric")],
+          a1_7[c("track_title", "lyric")])%>%
+    mutate(album = "Meet Miley Cyrus")
+
+# 2. Breakout 2008
+
+
+t2 = genius_album(artist = "Miley Cyrus", album = "Breakout")
+pend_t2 = filter(t2, is.na(line))
+
+pend_song =  genius_tracklist(artist = "Miley Cyrus", album = "Breakout")%>% 
+  filter(track_title %in% pend_t2$track_title)
+
+a1_1 = genius_url(pend_song$track_url[1], info = "all")
+a1_2 = genius_url(pend_song$track_url[2], info = "all")
+a1_3 = genius_url(pend_song$track_url[3], info = "all")
+
+a1_4 = genius_url(pend_song$track_url[4], info = "all")
+a1_5 = genius_url(pend_song$track_url[5], info = "all")
+a1_6 = genius_url(pend_song$track_url[6], info = "all")
+a1_7 = genius_url(pend_song$track_url[7], info = "all")
+a1_8 = genius_url(pend_song$track_url[8], info = "all")
+
+BO = bind_rows(t2[c("track_title", "lyric")], 
+               a1_1[c("track_title", "lyric")], a1_2[c("track_title", "lyric")],
+               a1_3[c("track_title", "lyric")])%>%
+  mutate(album = "Breakout")
+
+# 2. Breakout 2008
+
+#creando una funciona para los otros dicos
+
+song_url = function(alb){
+  
+  let = genius_album(artist = "Miley Cyrus", album = alb)
+  
+  pend_let = filter(let, is.na(line)) # obtengo las canciones que no tienen letras
+  
+  x =  genius_tracklist(artist = "Miley Cyrus", album = alb)%>% 
+    filter(track_title %in% pend_let$track_title) 
+  return(x)
+}
+
+
+pend_song = song_url("Breakout")
+
+a1_1 = genius_url(pend_song$track_url[1], info = "all")
+a1_2 = genius_url(pend_song$track_url[2], info = "all")
+a1_3 = genius_url(pend_song$track_url[3], info = "all")
+a1_4 = genius_url(pend_song$track_url[4], info = "all")
+a1_5 = genius_url(pend_song$track_url[5], info = "all")
+a1_6 = genius_url(pend_song$track_url[6], info = "all")
+a1_7 = genius_url(pend_song$track_url[7], info = "all")
+
+BO = bind_rows(t1[c("track_title", "lyric")], 
+               a1_1[c("track_title", "lyric")], a1_2[c("track_title", "lyric")],
+               a1_3[c("track_title", "lyric")], a1_4[c("track_title", "lyric")],
+               a1_5[c("track_title", "lyric")], a1_6[c("track_title", "lyric")],
+               a1_7[c("track_title", "lyric")])%>%
+  mutate(album = "Breakout")
+
+
+# i = genius_url("https://genius.com/Miley-cyrus-right-here-lyrics", info = "all")
+
+
+
+
+# evaluando lyrics
+unique(t2$track_title)
+summary(t3)
+
+
+
+
+HM_df = HM %>%
+  unnest_tokens(word, lyric)
+
+BO_df = BO %>%
   unnest_tokens(word, lyric) #paso Lyric palabra por palabra a la nueva columna word
 
-#contamos la frecuencia de palabras que encontramos
-MC_1 %>%
-  count(word, sort = TRUE) #muchas stopwords
 
-head(stop_words) #lista de stopwords de tidytext
+
 
 #quito la lista de stopwords a las canciones
-MC_2 = MC_1 %>%
+HM_df = HM_df %>%
   anti_join(stop_words)
 
-MC_2 %>%
+BO_df = BO_df %>%
+  anti_join(stop_words) %>%
+  filter(word != "la",word != "da", word != "sha", word != "yeah")
+
+#contamos la frecuencia de palabras que encontramos
+HM_df %>%
+  count(word, sort = TRUE)
+
+BO_df %>%
+  count(word, sort = TRUE)
+
+
+HM_df %>%
   count(word, sort = TRUE)%>% #algunas letras que no van como la da
-  filter(n > 9, word != "la",word != "da" )%>%
+  filter(n > 10, word != "la",word != "da",word != "hoo",word != "yeah")%>%
   ggplot(aes(x = reorder(word, n), y = n))+
   geom_col()+
   geom_text(aes(label = reorder(n, n)), 
@@ -75,7 +149,17 @@ MC_2 %>%
             size = 5)+
   coord_flip()
 
+tay_sentiment <- HM_df %>%
+  inner_join(get_sentiments("bing"))%>% 
+  count(album, track_title, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
 
+bo_sentimiento =   BO_df %>%
+  inner_join(get_sentiments("bing"))%>% 
+  count(album, track_title, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
 
 
 ### comprobando otras canciones en español ##########
