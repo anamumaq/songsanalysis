5266e2814be80818c34e5d402016af67a563a939
anamumaq <ana.munozmaquera@gmail.com>
2021-05-23 23:42:23 -0500

    Graficos y cuadro de sentimientos de los 3 primeros discos

--- script.R
+++ script.R
@@ -14,6 +14,7 @@ library(tidytext) #for text tidying
 # 2. Breakout 2008
 # 1. Hannah Montana 2: Meet Miley Cyrus 2007
 
+#################################################################################################################
 
 # 1. Hannah Montana 2: Meet Miley Cyrus 2007
 
@@ -24,28 +25,23 @@ t1 = genius_album(artist = "Miley Cyrus", album = "Meet Miley Cyrus")
 
 pend_t1 = filter(t1, is.na(line)) # obtengo las canciones que no tienen letras
 
-pend_song =  genius_tracklist(artist = "Miley Cyrus", album = "Meet Miley Cyrus")%>% 
+pend_songt1 =  genius_tracklist(artist = "Miley Cyrus", album = "Meet Miley Cyrus")%>% 
   filter(track_title %in% pend_t1$track_title) # obtengo url de las canciones sin letras
 
 # obtengo las letras de las canciones que faltaban uno a uno
-a1_1 = genius_url(pend_song$track_url[1], info = "all")
-a1_2 = genius_url(pend_song$track_url[2], info = "all")
-a1_3 = genius_url(pend_song$track_url[3], info = "all")
-a1_4 = genius_url(pend_song$track_url[4], info = "all")
-a1_5 = genius_url(pend_song$track_url[5], info = "all")
-a1_6 = genius_url(pend_song$track_url[6], info = "all")
-a1_7 = genius_url(pend_song$track_url[7], info = "all")
+a2_1 = genius_url(pend_songt1$track_url[1], info = "all")
+a2_2 = genius_url(pend_songt1$track_url[2], info = "all")
+a2_3 = genius_url(pend_songt1$track_url[3], info = "all")
+
 
 # uno los data sets en un solo llamado como el albun
 HM = bind_rows(t1[c("track_title", "lyric")], 
-          a1_1[c("track_title", "lyric")], a1_2[c("track_title", "lyric")],
-          a1_3[c("track_title", "lyric")], a1_4[c("track_title", "lyric")],
-          a1_5[c("track_title", "lyric")], a1_6[c("track_title", "lyric")],
-          a1_7[c("track_title", "lyric")])%>%
+               a2_1[c("track_title", "lyric")],
+               a2_2[c("track_title", "lyric")],
+               a2_3[c("track_title", "lyric")])%>%
     mutate(album = "Meet Miley Cyrus")
 
 # 2. Breakout 2008
-
 
 t2 = genius_album(artist = "Miley Cyrus", album = "Breakout")
 pend_t2 = filter(t2, is.na(line))
@@ -56,7 +52,6 @@ pend_song =  genius_tracklist(artist = "Miley Cyrus", album = "Breakout")%>%
 a1_1 = genius_url(pend_song$track_url[1], info = "all")
 a1_2 = genius_url(pend_song$track_url[2], info = "all")
 a1_3 = genius_url(pend_song$track_url[3], info = "all")
-
 a1_4 = genius_url(pend_song$track_url[4], info = "all")
 a1_5 = genius_url(pend_song$track_url[5], info = "all")
 a1_6 = genius_url(pend_song$track_url[6], info = "all")
@@ -70,6 +65,7 @@ BO = bind_rows(t2[c("track_title", "lyric")],
 
 # 2. Breakout 2008
 
+############### 
 #creando una funciona para los otros dicos
 
 song_url = function(alb){
@@ -94,23 +90,57 @@ a1_5 = genius_url(pend_song$track_url[5], info = "all")
 a1_6 = genius_url(pend_song$track_url[6], info = "all")
 a1_7 = genius_url(pend_song$track_url[7], info = "all")
 
-BO = bind_rows(t1[c("track_title", "lyric")], 
-               a1_1[c("track_title", "lyric")], a1_2[c("track_title", "lyric")],
-               a1_3[c("track_title", "lyric")], a1_4[c("track_title", "lyric")],
-               a1_5[c("track_title", "lyric")], a1_6[c("track_title", "lyric")],
-               a1_7[c("track_title", "lyric")])%>%
-  mutate(album = "Breakout")
+
+
+
+##################
+
 
 
 # i = genius_url("https://genius.com/Miley-cyrus-right-here-lyrics", info = "all")
 
 
 
+##########################################################################################################
 
-# evaluando lyrics
-unique(t2$track_title)
-summary(t3)
+t = genius_album(artist = "Miley Cyrus", album = "Bangerz")
 
+unique(t$track_title)
+
+pend = filter(t, is.na(line)) # obtengo las canciones que no tienen letras
+
+pend_song =  genius_tracklist(artist = "Miley Cyrus", album = "Bangerz")%>% 
+  filter(track_title %in% pend$track_title) # obtengo url de las canciones sin letras
+
+# obtengo las letras de las canciones que faltaban uno a uno
+a2_1 = genius_url(pend_song$track_url[1], info = "all")
+a2_2 = genius_url(pend_song$track_url[2], info = "all")
+a2_3 = genius_url(pend_song$track_url[3], info = "all")
+a2_4 = genius_url(pend_song$track_url[4], info = "all")
+a2_5 = genius_url(pend_song$track_url[5], info = "all")
+a2_6 = genius_url(pend_song$track_url[6], info = "all")
+a2_7 = genius_url(pend_song$track_url[7], info = "all")
+
+
+# uno los data sets en un solo llamado como el albun
+
+
+bgrz = bind_rows(t[c("track_title", "lyric")], 
+              a2_1[c("track_title", "lyric")],
+              a2_2[c("track_title", "lyric")],
+              a2_3[c("track_title", "lyric")],
+              a2_4[c("track_title", "lyric")],
+              a2_5[c("track_title", "lyric")],
+              a2_6[c("track_title", "lyric")],
+#              a2_7[c("track_title", "lyric")]
+#              ,a2_8[c("track_title", "lyric")]
+               )%>%
+  mutate(album = "Bangerz")
+
+
+
+
+#Separa las frases a palabras
 
 
 
@@ -121,15 +151,43 @@ BO_df = BO %>%
   unnest_tokens(word, lyric) #paso Lyric palabra por palabra a la nueva columna word
 
 
+TM_df = tm %>%
+  unnest_tokens(word, lyric)
 
+CNT_df = cant %>% ### arreglar
+  unnest_tokens(word, lyric)
+
+BGRZ_df = bgrz %>%
+  unnest_tokens(word, lyric)
 
 #quito la lista de stopwords a las canciones
+
 HM_df = HM_df %>%
-  anti_join(stop_words)
+  anti_join(stop_words)%>%
+  filter(!word %in% c("1", "4", "6", "9","a.m", "yeah","da","sha","la",'hoo'  ))
 
 BO_df = BO_df %>%
   anti_join(stop_words) %>%
-  filter(word != "la",word != "da", word != "sha", word != "yeah")
+  filter(!word %in% c("1", "4", "6", "9","a.m", "yeah","da","sha","la"  ))
+
+TM_df = TM_df %>%
+  anti_join(stop_words) %>%
+  filter(!word %in% c("1", "4", "3", "6", "9","a.m", "yeah","da","sha","la","u.s.a"  ))
+
+CNT_df = CNT_df %>%
+  anti_join(stop_words) %>%
+  filter(!word %in% c("1", "4", "3", "6", "9","a.m", "yeah","da","sha","la",
+                      "u.s.a","24","ya","yep","li","dnadon't","sh","hey"))
+
+
+BGRZ_df = BGRZ_df %>%
+  anti_join(stop_words) %>%
+  filter(!word %in% c("1", "4", "3", "6", "9","a.m", "yeah","da","sha","la",
+                      "u.s.a","24","ya","yep","li","dnadon't","sh","hey", "3d",
+                      "ay", "b.i.g","bangerz","yup","yell","ya'l","woo","whoa",
+                      "what'd","uh", "uhm"))
+
+
 
 #contamos la frecuencia de palabras que encontramos
 HM_df %>%
@@ -138,10 +196,24 @@ HM_df %>%
 BO_df %>%
   count(word, sort = TRUE)
 
+TM_df %>%
+  count(word, sort = TRUE)
 
-HM_df %>%
+CNT_df %>%
+  count(word, sort = TRUE)
+
+BGRZ_df %>%
+  count(word, sort = TRUE)
+
+# graficos
+
+CNT_df %>%
   count(word, sort = TRUE)%>% #algunas letras que no van como la da
-  filter(n > 10, word != "la",word != "da",word != "hoo",word != "yeah")%>%
+  filter(n>5,!word %in% c("1", "4", "3", "6", "9","a.m", "yeah","da","sha","la",
+                          "u.s.a","24","ya","yep","li","dnadon't","sh","hey", "3d",
+                          "ay", "b.i.g","bangerz","yup","yell","ya'l","woo","whoa",
+                          "what'd","uh", "uhm"),
+         word != is.na(word))%>%
   ggplot(aes(x = reorder(word, n), y = n))+
   geom_col()+
   geom_text(aes(label = reorder(n, n)), 
@@ -149,7 +221,9 @@ HM_df %>%
             size = 5)+
   coord_flip()
 
-tay_sentiment <- HM_df %>%
+# Sentimientos
+
+HM_sentimento = HM_df %>%
   inner_join(get_sentiments("bing"))%>% 
   count(album, track_title, sentiment) %>%
   spread(sentiment, n, fill = 0) %>%
@@ -160,6 +234,56 @@ bo_sentimiento =   BO_df %>%
   count(album, track_title, sentiment) %>%
   spread(sentiment, n, fill = 0) %>%
   mutate(sentiment = positive - negative)
+
+TM_sentimiento =   TM_df %>%
+  inner_join(get_sentiments("bing"))%>% 
+  count(album, track_title, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+CNT_sentimiento =   CNT_df %>%
+  inner_join(get_sentiments("bing"))%>% 
+  count(album, track_title, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+BGRZ_sentimiento =   BGRZ_df %>%
+  inner_join(get_sentiments("bing"))%>% 
+  count(album, track_title, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+#Joins los albums
+
+MC_alb = bind_rows(BGRZ_sentimiento, CNT_sentimiento, TM_sentimiento, bo_sentimiento,HM_sentimento)
+
+#plot
+
+MC_alb%>%
+  filter(album %in% c("Bangerz", "Breakout"))%>%
+  ggplot(aes(reorder(track_title, sentiment), sentiment, fill = album)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~album, scales = "free")+
+  labs(x = NULL,
+       y = "Sentiment",
+       title = "Bangerz & Breakout by MC Sentimental Analysis - p1",
+       caption = "                  by: Anamumaq")+
+  geom_text(aes(label = sentiment), 
+             hjust = 1.2,vjust = 0.3, color = "black", 
+             size = 3)+
+  theme_minimal()+
+  
+#     theme(plot.title = element_text(size = 13, hjust = 0.4, face = "bold"),
+#      axis.title.y = element_text(hjust = 0.05, size = 7, color = "grey40", angle = 0),
+#        axis.title.x =  element_text(size = 8, color = "grey40"),
+#        axis.text.x = element_text(size = 6.5, color = "grey40"),
+#        axis.text.y = element_text(size = 6.5, color = "grey40"), 
+#        strip.text = element_text(size = 9, color = "grey40", face = "bold"),
+#       plot.caption = element_text(size = 7.5, color = "grey40"))+
+  coord_flip()
+#       ggsave("sentiment.png", width = 10, height = 5.5)
+
+
 
 
 ### comprobando otras canciones en español ##########
